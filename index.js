aa4989c7854514f05d7a7c620f4e170ec0f821ec
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-06-10 22:50:05 +0600

    connect mongodb and create api for projects

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,9 +11,33 @@ app.use(express.json());
 
 app.get('/download-resume', function (req, res) {
   const file = `${__dirname}/files/MD_SAIF_ULLAH.pdf`;
-  console.log(file);
   res.download(file);
 });
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PASSWORD}@cluster0.eae4u.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const projectCollection = client.db('portfolio').collection('projects');
+
+    // get all projects
+    app.get('/project', async (req, res) => {
+      const result = await projectCollection.find({}).toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running..`);
